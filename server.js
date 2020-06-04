b37dc79f854ec57f64e8cae7f32b8130a49c90bd
suyashsorte <sorte.suyash@gmail.com>
2020-06-03 20:04:28 -0400

    Changed path of favicon.png

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ mongoose
 //     console.log('ERROR:', err);
 //   });
 // const port = process.env.PORT || 3000;
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`running on port ${port}...`);
 });
