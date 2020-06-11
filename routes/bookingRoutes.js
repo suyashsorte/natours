de3f28c6cd0f11b7607455d90d14738515c9f3ac
suyashsorte <sorte.suyash@gmail.com>
2020-06-10 21:28:56 -0400

    Added API routes for bookings

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,10 +3,20 @@ const express = require('express');
 const authcontroller = require('./../controllers/authController');
 const router = express.Router();
 
-router.get(
-  '/checkout-session/:tourId',
-  authcontroller.protect,
-  bookingController.getCheckoutSession
-);
+router.use(authcontroller.protect);
+
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+
+router.use(authcontroller.restrictTo('admin', 'lead-guide'));
+router
+  .route('/')
+  .get(bookingController.getAllBooking)
+  .post(bookingController.createBooking);
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = router;
