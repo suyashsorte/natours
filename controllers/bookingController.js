9351b71c02a518a422f5f8aad317a85378ebbb97
suyashsorte <sorte.suyash@gmail.com>
2020-06-11 22:49:17 -0400

    testing stripe API key error

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,15 +1,16 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('./../models/tourModel');
 const User = require('../models/userModel');
 const Booking = require('./../models/bookingModel');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('./../utils/appError');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
-  console.log('booking controller', req.params.tourId);
+
   const tour = await Tour.findById(req.params.tourId);
+  // console.log('booking controller', req.params.tourId);
 
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
